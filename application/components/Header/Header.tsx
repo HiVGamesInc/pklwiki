e8de7aac017a591a291a60411d536deb56ce0dd5
André Tronca <andretronca@gmail.com>
2023-05-19 11:14:16 -0300

    feature(core): add prettier and logo (#6)

--- application/components/Header/Header.tsx
+++ application/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
-import { MagnifyingGlassIcon, MoonIcon, SunIcon } from '@heroicons/react/24/solid'
+import {
+  MagnifyingGlassIcon,
+  MoonIcon,
+  SunIcon
+} from "@heroicons/react/24/solid";
 import Icon from "components/Icon/Icon";
 import Input from "components/Input/Input";
 import { useThemeContext } from "context/theme";
@@ -9,12 +13,22 @@ export default function Header() {
   const DarkModeIcon = !darkMode ? MoonIcon : SunIcon;
 
   return (
-    <header className={`w-full bg-bgPrimary shadow-md border-b ${darkMode ? 'border-b-gray-800' : 'border-b-gray-100'}`}>
+    <header
+      className={`w-full bg-bgPrimary shadow-md border-b ${
+        darkMode ? "border-b-gray-800" : "border-b-gray-100"
+      }`}
+    >
       <span className="flex items-center justify-between w-full max-w-screen-xl mx-auto h-16 p-4">
-        <img src="https://placehold.co/200x50.png" alt="Logo" className="h-full"/>
+        <img
+          src="https://placehold.co/200x50.png"
+          alt="Logo"
+          className="h-full"
+        />
         <div className="flex items-center relative">
-          <Input value='' />
-          <button className="h-4 block text-dBlack absolute top-1/2 right-0 translate-x-[-50%] translate-y-[-50%]"><Icon icon={MagnifyingGlassIcon} /></button>
+          <Input value="" />
+          <button className="h-4 block text-dBlack absolute top-1/2 right-0 translate-x-[-50%] translate-y-[-50%]">
+            <Icon icon={MagnifyingGlassIcon} />
+          </button>
         </div>
         <div className="flex items-center">
           {/* <button className="h-4 block text-dBlack" onClick={() => setDarkMode(!darkMode)}><Icon icon={DarkModeIcon} /></button> */}
@@ -22,5 +36,5 @@ export default function Header() {
         </div>
       </span>
     </header>
-  )
+  );
 }
--- application/components/Icon/Icon.tsx
+++ application/components/Icon/Icon.tsx
@@ -1,8 +1,10 @@
 type IconType = {
-  icon?: React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
-      title?: string | undefined;
-      titleId?: string | undefined;
-  } & React.RefAttributes<SVGSVGElement>>
+  icon?: React.ForwardRefExoticComponent<
+    Omit<React.SVGProps<SVGSVGElement>, 'ref'> & {
+      title?: string | undefined
+      titleId?: string | undefined
+    } & React.RefAttributes<SVGSVGElement>
+  >
   img?: string
   size?: number
 }
@@ -10,7 +12,9 @@ type IconType = {
 export default function Icon(props: IconType) {
   const style = { height: `${props.size || 1}em`, marginRight: '.4em' }
 
-  return (
-    props.icon ? <props.icon style={style} /> : props.img ? <img src={props.img} style={style} /> : null
-  )
+  return props.icon ? (
+    <props.icon style={style} />
+  ) : props.img ? (
+    <img src={props.img} style={style} />
+  ) : null
 }
